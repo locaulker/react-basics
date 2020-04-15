bdb893470651d812d47b2e7008cc8beb28f31e1b
Lawrence <locaulker@gmail.com>
2020-04-15 06:52:36 -0400

    initial commit with nested components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react"
+import ReactDOM from "react-dom"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Books() {
+	return (
+		<section>
+			<Book />
+			<Book />
+			<Book />
+			<Book />
+			<Book />
+		</section>
+	)
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Explicit return
+const Book = () => {
+	return (
+		<article>
+			<CoverImage />
+			<Title />
+			<Author />
+		</article>
+	)
+}
+
+// Implicit return
+const CoverImage = () => (
+	<img
+		width="200"
+		src="https://m.media-amazon.com/images/I/71N4oeWwYlL._AC_UY327_FMwebp_QL65_.jpg"
+		alt="Wonky Donkey"
+	/>
+)
+
+const Title = () => <h1>The Wonky Donkey</h1>
+const Author = () => <p>by Craig Smith</p>
+
+ReactDOM.render(<Books />, document.getElementById("root"))
